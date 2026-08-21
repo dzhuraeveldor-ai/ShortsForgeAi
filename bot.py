efec84f7dfea506f8ad11b7c123d4fcb5ef13267
dzhuraeveldor-ai <dzhuraeveldor@gmail.com>
2026-08-21 17:41:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
-BOT_TOKEN = "8984766153:AAGl5sHCbXT22DQoOvOP7vgPabp5j7fGzKI"
+BOT_TOKEN = "8984766153:AAHd7Pyvgw6dUlf11RnGcTcGI-RMRt8QVow"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
