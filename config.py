f8c5698b63c41ca64ea0d40651b79e13807dfb7d
dzhuraeveldor-ai <dzhuraeveldor@gmail.com>
2026-08-23 11:44:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-settings = {
+bot_settings = {
     "BOT_TOKEN": os.getenv("BOT_TOKEN", "8984766153:AAGKVpwZkPeMYmByrY69o-gBFO3ZE6vaJzE"),
     "ADMIN_ID": os.getenv("ADMIN_ID", "8941864145"),
     "WORKER_API_URL": os.getenv("WORKER_API_URL", "http://localhost:8000")
